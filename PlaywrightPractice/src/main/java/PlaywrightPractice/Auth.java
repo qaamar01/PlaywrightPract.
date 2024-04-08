f7b780d5d9394c6957192d774140542cf0ae9640
Amar <amarksingh09@gmail.com>
2024-04-08 10:26:54 +0530

    Commiting with updated classes.

--- PlaywrightPractice/src/main/java/PlaywrightPractice/Auth.java
+++ PlaywrightPractice/src/main/java/PlaywrightPractice/Auth.java
@@ -1,5 +1,8 @@
 package PlaywrightPractice;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserContext;
 import com.microsoft.playwright.BrowserType;
@@ -8,19 +11,40 @@ import com.microsoft.playwright.Playwright;
 
 public class Auth {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		Playwright playwright = Playwright.create();
 		Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-		BrowserContext br1 = browser.newContext();
+		
+//		BrowserContext br1 = browser.newContext();
+//		Page page = br1.newPage();
+//		page.navigate("https://dev.admin.botshot.ai/auth");
+//		page.click("input#mat-input-0");
+//		page.fill("input#mat-input-0", "amar.singh@mailinator.com");
+//		page.click("input#current-password");
+//		page.fill("input#current-password", "12345678");
+//		page.click("//button[@type='submit']");
+//		page.click("div.container div.btn.btn__yes");
+	
+		
+		// To store login data...................................
+		//br1.storageState(new BrowserContext.StorageStateOptions().setPath(Paths.get("adminLogin.json")));  
+	
+		
+		
+		//To fetch saved login JSON file.............................
+		BrowserContext br1=browser.newContext(new Browser.NewContextOptions().setStorageStatePath(Paths.get("adminLogin.json"))); 
 		Page page = br1.newPage();
 		page.navigate("https://dev.admin.botshot.ai/auth");
-		page.click("input#mat-input-0");
-		page.fill("input#mat-input-0", "amar.singh@mailinator.com");
-		page.click("input#current-password");
-		page.fill("input#current-password", "12345678");
-		page.click("//button[@type='submit']");
-		page.click("div.container div.btn.btn__yes");
+		Thread.sleep(5000);
+        page.click("div.container div.btn.btn__yes");
+        Thread.sleep(3000);
+        page.click("div.menu_button");
+        Thread.sleep(2000);
+        page.click("div.menu.ng-star-inserted div:has-text('eFront Desk')");
+        
+        
+        
 	
 	}
-
+	
 }
